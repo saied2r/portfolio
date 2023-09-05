807801b8ba08ad7ed077c2079ae8d2fd0b98aff9
saied2r <saiedtechit@gmail.com>
2023-09-05 22:55:40 +0600

    fix navbar for responsivity

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
+
 document.getElementById("menu").addEventListener("click", menuOpen);
 
-document.getElementById("myLinks").addEventListener("click", ()=>{
-document.getElementById("myLinks").style.display = "none";
-document.getElementById("social").style.display = "none";
+document.getElementById("myLinks").addEventListener("click", () => {
+  document.getElementById("myLinks").style.display = "none";
+  document.getElementById("social").style.display = "none";
 })
-document.getElementById("social").addEventListener("click", ()=>{
-document.getElementById("myLinks").style.display = "none";
-document.getElementById("social").style.display = "none";
+document.getElementById("social").addEventListener("click", () => {
+  document.getElementById("myLinks").style.display = "none";
+  document.getElementById("social").style.display = "none";
 })
 
 function menuOpen() {
-  let x = document.getElementById("myLinks");
-  let y = document.getElementById("social");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-  if (y.style.display == "flex"){
+  if (window.innerWidth < 768) {
+    let x = document.getElementById("myLinks");
+    let y = document.getElementById("social");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    if (y.style.display == "flex") {
       y.style.display = "none";
-  }else{
-    y.style.display = "flex";
+    } else {
+      y.style.display = "flex";
+    }
   }
+
 }
+
 
 
 
